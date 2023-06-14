9010c0dbff3a54f72e289b9322a8874ba1dd8220
Ok Hyun Lee <OkHyun.Lee@coxautoinc.com>
2023-06-14 18:53:20 -0400

    adding postgresql & JPA

---
+++ src/main/java/com/example/demo/student/studentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.util.Calendar.APRIL;
+
+@Configuration
+public class studentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+           Student mariam = new Student(
+                    "Olee",
+                    "hellow@gmail.com",
+                    LocalDate.of(2000, APRIL, 5),
+                    21);
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, APRIL, 5),
+                    22);
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+
+    }
+}
