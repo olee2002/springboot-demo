ff61845a44a3c1aece0141ff35746651cbcc78c6
Ok Hyun Lee <OkHyun.Lee@coxautoinc.com>
2023-06-14 16:32:46 -0400

    initial spring boot set up n student service

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/v1/student")
+class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+
+
+}
